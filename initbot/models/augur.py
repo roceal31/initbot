5bd8100e56cad9d1f5f5f47b82a59f96106bf32f
Andrea James <roceal@jngm.net>
2022-09-24 10:32:06 +1000

    Deletes original initbot files from repo due to decision to split off initbot from soundboard bot

--- initbot/models/augur.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class AugurModel(BaseModel):
-    description: str
-    roll: int
--- initbot/models/occupation.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-
-
-class OccupationModel(BaseModel):
-    rolls: List[int]
-    name: str
-    weapon: str
-    goods: str
